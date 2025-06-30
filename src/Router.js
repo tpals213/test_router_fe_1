6271c81781c891b4fa598c51e122c730e894bbc5
tpals213 <tpals213@gmail.com>
2025-06-30 14:34:13 +0900

    변경

--- src/Router.js
+++ src/Router.js
@@ -7,7 +7,7 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/test/router" element={<TestRouter />} />
+        <Route path="" element={<TestRouter />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/TestRouter/TestRouter.jsx
+++ src/pages/TestRouter/TestRouter.jsx
@@ -35,6 +35,20 @@ const TestRouter = () => {
                             fastAPI 통신
                         </button>
                     </div>
+                    <div className='pt-8 '>
+                        <p className='text-2xl'>1. 사용자가 질문 입력 후 답변 받기 라는 버튼 클릭 후 해당 답변 GPT 로 받아보기 </p>
+                        <p>fastAPI 는 엔드포인트 - 서비스 레이어 까지 통신 </p>
+                        <p>답변은 answer 라고 지정 </p>
+                        <p>옆 nav 바에 gpt 대답으로 등록 - 경로는 알아서 </p>
+                        <p>심화 : isLoading 으로 로딩 처리 해보기 </p>
+                    </div>
+                    <div className='pt-8 '>
+                        <p className='text-2xl'>2. 사용자가 숫자 입력 후 해당하는 시/도 명 가져오기 </p>
+                        <p>fastAPI 는 엔드포인트 - 서비스 - crud 레이어 까지 통신 </p>
+                        <p>답변은 city 라고 지정 </p>
+                        <p>옆 nav 바에 시/도명 조회로 등록 - 경로는 알아서 </p>
+                        <p>숫자 형태로 입력 되어야하며 숫자는 1~ 17 로 제한 </p>
+                    </div>
                 </main>
             </div>
         </div >
